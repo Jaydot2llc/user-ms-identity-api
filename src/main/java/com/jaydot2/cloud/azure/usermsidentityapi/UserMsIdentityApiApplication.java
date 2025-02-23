490da43288b3d08091c1127146b69d3b31e5e91c
James Bray <jrbrayjr2006@gmail.com>
2025-02-23 13:39:49 -0600

    first commit

---
+++ src/main/java/com/jaydot2/cloud/azure/usermsidentityapi/UserMsIdentityApiApplication.java
@@ -0,0 +1,13 @@
+package com.jaydot2.cloud.azure.usermsidentityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsIdentityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMsIdentityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaydot2/cloud/azure/usermsidentityapi/UserMsIdentityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaydot2.cloud.azure.usermsidentityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMsIdentityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
